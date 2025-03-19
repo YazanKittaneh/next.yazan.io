309366296d5e9a0adb2a6355b4ca912546044464
Yazan Kittaneh <yazanakittaneh@gmail.com>
2025-03-19 17:47:45 -0500

    adding files

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Dashboard from "@/components/dashboard";
+
+/**
+ * The main page component that renders the HomePage component.
+ *
+ * @returns {JSX.Element} The rendered HomePage component.
+ */
+const Page = () => {
+    return (
+        <Dashboard></Dashboard>
+    )
+};
+
+export default Page;
